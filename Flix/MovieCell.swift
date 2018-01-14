74fc3ff49771d3b1b2b58c3346507b372ef592d7
QD <qd@QDs-MacBook-Pro.local>
2018-01-14 00:06:34 -0500

    Required stories

---
+++ Flix/MovieCell.swift
@@ -0,0 +1,24 @@
+//
+//  MovieCell.swift
+//  Flix
+//
+//  Created by QD on 1/13/18.
+//  Copyright © 2018 Qingdong. All rights reserved.
+//
+
+import UIKit
+
+class MovieCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
